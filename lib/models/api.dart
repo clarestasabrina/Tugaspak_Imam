155cf7b090c723bced2452b4b8b82a3e7e2a694b
clarestasabrina <clarerasintiya@gmail.com>
2024-10-27 15:02:06 +0700

    Add files via upload

---
+++ lib/models/api.dart
@@ -0,0 +1,7 @@
+class BaseUrl{
+  static String tambah = "http://192.168.203.155/biodata/create.php";
+  static String lihat = "http://192.168.203.155/biodata/details.php";
+  static String edit = "http://192.168.203.155/biodata/update.php";
+  static String hapus = "http://192.168.203.155/biodata/delete.php";
+  static String data = "http://192.168.203.155/biodata/list.php";
+}
